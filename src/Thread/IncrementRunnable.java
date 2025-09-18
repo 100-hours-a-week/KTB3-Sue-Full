6079774a7582fcb4c2fdbb4e7e140b7fe06390ee
osj1405 <osj1405@naver.com>
2025-09-18 15:32:13 +0900

    Runnable try

---
+++ src/Thread/IncrementRunnable.java
@@ -0,0 +1,15 @@
+package Thread;
+
+public class IncrementRunnable implements Runnable{
+    private final Counter counter;
+
+    public IncrementRunnable(Counter counter){
+        this.counter = counter;
+    }
+    @Override
+    public void run(){
+        for(int i = 0; i < 1000; i++){
+            counter.increment();
+        }
+    }
+}
--- src/Thread/Main.java
+++ src/Thread/Main.java
@@ -11,12 +11,22 @@ public class Main {
         IncrementThread thread1 = new IncrementThread(counter);
         IncrementThread thread2 = new IncrementThread(counter);
 
+        Runnable task1 = new IncrementRunnable(counter);
+        Runnable task2 = new IncrementRunnable(counter);
+        Thread thread3 = new Thread(task1);
+        Thread thread4 = new Thread(task2);
+
         try {
             thread1.start();
             thread2.start();
 
+            thread3.start();
+            thread4.start();
+
             thread1.join();
             thread2.join();
+            thread3.join();
+            thread4.join();
         }catch(InterruptedException e) {
             System.out.println(e.getMessage());
         }
