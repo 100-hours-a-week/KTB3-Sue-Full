0c38fe19f5a6e591da2b8f20b4faf27089a11dda
osj1405 <osj1405@naver.com>
2025-09-19 21:51:57 +0900

    AtomicInteger try

--- src/Thread/Counter.java
+++ src/Thread/Counter.java
@@ -1,10 +1,17 @@
 package Thread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
     private int value;
+    private AtomicInteger atomicValue = new AtomicInteger(0);
 
-    public void increment(){
+    synchronized public void increment(){
         value++;
+    }
+
+    public void atomicIncrement(){
+        int newValue = atomicValue.incrementAndGet();
     }
 
     public int getValue(){
--- src/Thread/IncrementRunnable.java
+++ src/Thread/IncrementRunnable.java
@@ -9,7 +9,8 @@ public class IncrementRunnable implements Runnable{
     @Override
     public void run(){
         for(int i = 0; i < 1000; i++){
-            counter.increment();
+            // counter.increment();
+            counter.atomicIncrement();
         }
     }
 }
